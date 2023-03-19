f5108f2e8d81adcd8e2bdf3173e361afaa9f7ec6
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-03-19 19:56:17 +0200

    Create A Death Handler

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float playerHitPoints = 200f;
-
+    
     public void DecreasePlayerHealth(float damage)
     {
         playerHitPoints -= damage;
         if(playerHitPoints <= 0)
         {
-            Debug.Log("Player is dead");
+            GetComponent<DeathHandler>().HandleDeath();
         }
 
     }
