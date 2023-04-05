3aaf1fb98aa33dc4466e1b94196be8a181243f72
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-04-05 12:57:54 +0300

    Ammo Pickup - Part 2

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
+    
     private void OnTriggerEnter(Collider other) 
     {
         if(other.gameObject.tag == "Player")
         {
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
             Debug.Log("Player did what players do");
-            Destroy(gameObject)
+            Destroy(gameObject);
         }
     }
 }
